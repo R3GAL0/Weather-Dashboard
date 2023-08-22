12521d83b7777a405300cdbba1caee67c1f3bfbb
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-08-22 15:36:52 -0400

    api call works and prints to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,32 +16,33 @@ var keyAPI = '000d26729d4f39b6f39ce11ee8904c9b';
 
 //  http://api.openweathermap.org/geo/1.0/direct?q=Toronto&limit=1&appid=000d26729d4f39b6f39ce11ee8904c9b
 
-var city = 'London';
-var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+// var city = 'London';
+var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+// var apiURL = 'api.openweathermap.org/data/2.5/forecast?q=';
 
-var queryURL = apiURL + city + '&appid=' + keyAPI;
+// var queryURL = apiURL + city + '&appid=' + keyAPI;
 
-fetch(queryURL).then(response) {
+// fetch(queryURL).then(response) {
 
-    }
+//     }
 
 
 
-// populate the city list from storage
+// // populate the city list from storage
 var cityStor = JSON.parse(localStorage.getItem('cityStor')) || [];
 
-// structure
-cityStor[0] = {
-    city: 0; // contains city name
-    forecast: 0; // contains forcast info
-}
+// // structure
+// cityStor[0] = {
+//     city: 0; // contains city name
+//     forecast: 0; // contains forcast info
+// }
 
 
-function makeList (cityStor) {
-    if (cityStor.length == 0){
+function makeList(cityStor) {
+    if (cityStor.length == 0) {
         return;
     }
-    for (var i=0; i<cityStor.length; i++){
+    for (var i = 0; i < cityStor.length; i++) {
         var li = document.createElement("li");
         li.textContent = cityStor[i].city;
         li.setAttribute('data-cityIndex', cityStor[i].city);
@@ -54,3 +55,22 @@ makeList(cityStor);
 // search function
 // api call
 // display api info
+
+btnEl.addEventListener("click", function () {
+    var searchFor = cityEl.value;
+    // make api request
+    var queryURL = apiURL + searchFor + '&appid=' + keyAPI;
+    console.log(queryURL);
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // display results
+            console.log(data);
+            // store results
+
+        });
+    // update prev searched cities list
+    // makeList(cityStor);
+});
