29a82783cc0b14355b162828564b7119dcc732df
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-08-22 16:30:17 -0400

    can call one day of weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,38 +4,57 @@
 var cityEl = document.querySelector('#city');
 var btnEl = document.querySelector('.btn');
 var cityListEl = document.querySelector('#city-list');
-
+var todayEl = document.querySelector('#weather-today')
 // API Key
 var keyAPI = '000d26729d4f39b6f39ce11ee8904c9b';
-
-
-// get today + 5 day forcast for a city
-// store the city + forecast in local storage
-
+var cityStor = JSON.parse(localStorage.getItem('cityStor')) || [];
 
 
 //  http://api.openweathermap.org/geo/1.0/direct?q=Toronto&limit=1&appid=000d26729d4f39b6f39ce11ee8904c9b
-
-// var city = 'London';
 var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 // var apiURL = 'api.openweathermap.org/data/2.5/forecast?q=';
 
-// var queryURL = apiURL + city + '&appid=' + keyAPI;
 
-// fetch(queryURL).then(response) {
-
-//     }
-
-
-
-// // populate the city list from storage
-var cityStor = JSON.parse(localStorage.getItem('cityStor')) || [];
-
-// // structure
-// cityStor[0] = {
-//     city: 0; // contains city name
-//     forecast: 0; // contains forcast info
-// }
+// structure
+cityStor[0] = {
+    city: 0, // contains city name
+    day1: {
+        clouds: 0, 
+        temp: 0,
+        wind: 0,
+        humidity: 0,
+    },
+    day2: {
+        clouds: 0, 
+        temp: 0,
+        wind: 0,
+        humidity: 0,
+    },
+    day3: {
+        clouds: 0, 
+        temp: 0,
+        wind: 0,
+        humidity: 0,
+    },
+    day4: {
+        clouds: 0, 
+        temp: 0,
+        wind: 0,
+        humidity: 0,
+    },
+    day5: {
+        clouds: 0, 
+        temp: 0,
+        wind: 0,
+        humidity: 0,
+    },
+    day6: {
+        clouds: 0, 
+        temp: 0,
+        wind: 0,
+        humidity: 0,
+    },
+};
 
 
 function makeList(cityStor) {
@@ -58,6 +77,7 @@ makeList(cityStor);
 
 btnEl.addEventListener("click", function () {
     var searchFor = cityEl.value;
+    searchFor = 'Toronto'; // for ease of use in development
     // make api request
     var queryURL = apiURL + searchFor + '&appid=' + keyAPI;
     console.log(queryURL);
@@ -68,9 +88,21 @@ btnEl.addEventListener("click", function () {
         .then(function (data) {
             // display results
             console.log(data);
+            var today = dayjs().format(' (MM/DD/YYYY)');
+            console.log(today);
+            // todayEl.children.eq(0).textContent = searchFor + today;
+            $(todayEl).children().eq(0).text(searchFor + today);
+            $(todayEl).children().eq(1).append('<li>Cloud Cover: ' + data.weather[0].description + '</li>');
+            $(todayEl).children().eq(1).append('<li>Temp: ' + data.main.temp + ' °F</li>');
+            $(todayEl).children().eq(1).append('<li>Wind: ' + data.wind.speed + ' MPH</li>');
+            $(todayEl).children().eq(1).append('<li>Humidity: ' + data.main.humidity + ' %</li>');
             // store results
 
         });
+
+
     // update prev searched cities list
     // makeList(cityStor);
 });
+
+// if user performs a search refresh the page
